ea5be89fd2767e2e30c58992707adef4b6faf700
Lojan  Jayakumar <loji@Lojans-Laptop.local>
2024-07-16 20:55:01 +0530

    Alignment, screen responsiveness fix

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -1,48 +1,51 @@
 import './App.css'
 import {Card, CardActionArea, CardContent, CardMedia, Typography} from '../mui.component'
-import { CardActions, Container } from '@mui/material'
 
 function App() {
 
   return (
-    <>
-    <Card sx={{
-      width: '17.5%'
-    }}>
-      <CardActionArea sx={{padding: 1}}>
-        <CardMedia
-        component="img"
-        style={{borderRadius: '5%'}}
-        image='src/assets/images/image-qr-code.png'
-        />
-      </CardActionArea>
-        <CardContent>
-          <Typography 
-          gutterBottom 
-          align='center' 
-          component='div'
-          sx={{
-            margin:'20px',
-            lineHeight: 1.2,
-            fontFamily: 'Outfit, sans-serif',
-            fontWeight: 700, 
-            fontSize: '15px'
-          }}>
-          Improve your front-end skills by building projects
-          </Typography>
-          <Typography 
-          align='center'
-          sx={{
-            lineHeight: 1.2,
-            fontFamily: 'Outfit, sans-serif',
-            fontWeight: 200,
-            fontSize: '15px'
-          }}>
-          Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
-          </Typography>
-        </CardContent>
-    </Card>
-    </>
+      <Card
+      sx={{ 
+        border: 'none',
+        width: '17.5rem',
+        borderRadius: '15px',
+        boxShadow: 'none',
+      }}>
+        <CardActionArea sx={{padding:1.5}}>
+          <CardMedia
+          component="img"
+          style={{borderRadius: '5%'}}
+          image='src/assets/images/image-qr-code.png'
+          />
+        </CardActionArea> 
+          <CardContent>
+            <Typography 
+            gutterBottom 
+            align='center' 
+            component='div'
+            sx={{
+              lineHeight: 1.2,
+              fontFamily: 'Outfit, sans-serif',
+              fontWeight: 700, 
+              fontSize: '17.5px'
+            }}>
+            Improve your front-end skills by building projects
+            </Typography>
+            <Typography
+            align='center' 
+            sx={{
+              margin:'10px',
+              lineHeight: 1.2,
+              fontFamily: 'Outfit, sans-serif',
+              fontWeight: 50,
+              fontSize: '12px',
+              color: 'hsl(220, 15%, 55%)'
+            }}
+            >
+              Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+            </Typography>
+          </CardContent>
+      </Card>
   )
 }
 
